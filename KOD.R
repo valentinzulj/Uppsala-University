966c4f597d409352ff4d8823a786958fd914b74a
Valentin Zulj <43027502+valentinzulj@users.noreply.github.com>
2018-09-24 17:05:09 +0200

    Add files via upload

--- KOD.R
+++ KOD.R
@@ -1,3 +1,6 @@
+library(tidyverse)            
+
+
                 #####################################################
                 ################# Linear Regression #################
                 #####################################################
@@ -22,12 +25,12 @@ return(return_obj)
 }
 
 A <- data.frame(Y = rexp(100, rate =2), # Depentent variable
-                   X1 = rnorm(100), # Independent variable
+                   X1 = rnorm(100),     # Independent variable
                    X2 = rnorm(100, mean = 4, sd = 2) # Dependent variable
                 )
 
 lin_mod <- linear_regression(data = A, dep = 1, # y variable is first columnt in 'data'
-                  indep = c(2,3) # regressors are in columns two and three
+                  indep = c(2,3)                # regressors are in columns two and three
                             )
 
 ci <- function(lin_mod, pos, alfa){
@@ -61,8 +64,35 @@ int
                 ################# Stratified t-test #################
                 #####################################################
                   
-getwd()  
-setwd("U:\Downloads\Uppsala-University-Labs (1).zip\Uppsala-University-Labs")
-  
-  
+set.seed(2018)
+strat <- tibble(x = c(rnorm(200, 25), rnorm(200, 45), rnorm(200, 75)),
+                treatment = rep(1:2, 300),
+                strata = c(rep(1, 200), rep(2, 200), rep(3, 200)))
+
+
+strat <- strat %>%
+  group_by(treatment, strata) %>%
+  summarize(n = length(strata), #computing n
+            s2 = var(x), # Computing s-squared
+            m = mean(x)) %>% # Computing x-bar
+  group_by(strata) %>%
+  mutate(sprod = s2*n) #multiplying n by variance
+strat  
+
+
+for(i in unique(strat$strata)){
+strat %>%
+    filter(strata == i) %>%
+    summarize(nsum = sum(n),      # Summing number of obs
+              rnsum = sum(n) - 2, # Subtracting 2
+              ssum = sum(sprod),  # Summing the n-variance products
+              nprod = prod(n)) %>% # Multiplying the number of obs
+  print()
+}  
+
+
+
+
+
+
   
