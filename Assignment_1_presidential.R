5c1cc2c6dd39f22fe9667748de356e9f2ee8e484
Valentin Zulj <43027502+valentinzulj@users.noreply.github.com>
2018-09-26 11:49:33 +0200

    Add files via upload

--- Assignment_1_presidential.R
+++ Assignment_1_presidential.R
@@ -57,11 +57,14 @@ sammanslaget$gop_win <- as.factor(sammanslaget$gop_win) # Ändrar gap_wins class 
 
 
 #  Exploratory data analysis
-scatter1 <- ggplot(data = sammanslaget, mapping = aes(x = INC110213, y = per_gop_2016)) +
-  geom_point(color = "red", size = 2) +
+scatter1 <- ggplot(data = sammanslaget, mapping = aes(x = INC110213, y = per_gop_2016,
+                                                      color = HSD310213)) +
+  geom_point(size = 2) +
   geom_smooth(method = lm, se = TRUE, color = "blue", size = 1.5) +
-  scale_y_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80), labels = c("0%", "20%", "40%", "60%", "80%")) +
-  scale_x_continuous(breaks = c(25000, 50000, 75000, 100000, 125000), labels = c("$25000", "$50000", "$75000", "$100000", "$125000")) +
+  scale_y_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80), 
+                     labels = c("0%", "20%", "40%", "60%", "80%")) +
+  scale_x_continuous(breaks = c(25000, 50000, 75000, 100000, 125000), 
+                     labels = c("$25000", "$50000", "$75000", "$100000", "$125000")) +
   labs(title = "2016 US presidential election, county-level",
        subtitle = "Final result for the republican party explained by the median income",
        x = "Median household income, 2009-2013",
@@ -71,7 +74,8 @@ scatter1
 histogram1 <- ggplot(data = sammanslaget) + 
   geom_histogram(mapping = aes(x = per_gop_2016), fill = "red", alpha = 1) +
   geom_histogram(mapping = aes(x = per_gop_2012), fill = "blue", alpha = 0.5) +
-  scale_x_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80), labels = c("0%", "20%", "40%", "60%", "80%")) +
+  scale_x_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80), 
+                     labels = c("0%", "20%", "40%", "60%", "80%")) +
   annotate(geom="text", x=0.93, y=287, label="2016", color="red", size = 5) +
   annotate(geom="text", x=0.93, y=267, label="2012", color="blue", alpha = 0.5, size = 5) +
   labs(title = "2016 and 2012 US presidential election, county-level",
@@ -83,7 +87,8 @@ histogram1
 
 boxplot1 <- ggplot(sammanslaget, aes(x = gop_win, y = EDU685213)) +
   geom_boxplot() +
-  scale_y_continuous(breaks = c(0, 20, 40, 60, 80), labels = c("0%", "20%", "40%", "60%", "80%")) +
+  scale_y_continuous(breaks = c(0, 20, 40, 60, 80), 
+                     labels = c("0%", "20%", "40%", "60%", "80%")) +
   scale_x_discrete(breaks = c(0, 1), labels = c("Democrats", "Republicans")) +
   labs(title = "2016 US presidential election",
        subtitle = "Boxplot of the educational level",
@@ -91,3 +96,21 @@ boxplot1 <- ggplot(sammanslaget, aes(x = gop_win, y = EDU685213)) +
        y = "Bachelor's degree or higher, percent of persons age 25+, 2009-2013")
 
 boxplot1
+
+gamling <- ggplot(sammanslaget, aes(x = AGE775214, 
+                                    fill = gop_win,
+                                    color = gop_win)) + 
+  geom_density() +
+  facet_wrap(~gop_win, nrow = 2) +
+  labs(title = "Elderly votes in the 2016 Presidiential Election",
+       subtitle = "Histogram of the Republican party´s final result in 2016 and 2012",
+       x = " Persons aged 65 or over, %",
+       y = "Density",
+       fill = "Carried by") +
+  guides(color = FALSE) + # Removing legend for "color"
+  scale_fill_manual(labels = c("GOP", "Dems"), # Labelling "fill" legends
+                    values = c("red", "blue")) + # Using party colours for fill
+  scale_color_manual(values = c("red", "blue")) + # Using party colour for borders
+  theme(strip.background = element_blank(), # Removing facet_wrap labels
+    strip.text.x = element_blank())
+gamling
