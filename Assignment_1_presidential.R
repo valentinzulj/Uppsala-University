c41988f4dc84398ffe60d3e869455976b44d940d
vilgotosterlund <43535823+vilgotosterlund@users.noreply.github.com>
2018-09-26 10:37:25 +0200

    Add files via upload

--- Assignment_1_presidential.R
+++ Assignment_1_presidential.R
@@ -1,74 +1,74 @@
-library(tidyverse) # Needs to be installed first
-library(devtools) # Needs to be installed first
+library(tidyverse)
+library(devtools)
 library(StatProg)
 library(scales)
 
-
-# LÃ¤sa in dread csv() for comma-separated files
-# read csv2() for semicolon-separated files,
-# read tsv() for tab-separated files
-# read delim() for files with a general separator
-# read fwf() for files with fixed widths
-# read table() for files where white space separates columns
-# (read excel() for Excel files)ata
-
+# Data preparation
 crime_data <- read_tsv("crime_data.tsv")
 dictionary_county_facts <- read_csv("county_facts_dictionary.csv")
 county_facts <- read_csv("county_facts.csv")
 results <- read_csv("general_result.csv")
 
-
 results <- results %>%
-  arrange(combined_fips)
+  arrange(combined_fips) # Sorterar datatt efter fips i storleksordning, minst först
 
-colnames(results)[2] <- "fips"
+colnames(results)[2] <- "fips" # Byter namn så att alla ska va samma
 
-county_facts <- na.omit(county_facts, cols = "state_abbreviation")
-colnames(county_facts)[1] <- "fips"
-county_facts$fips <- as.character(county_facts$fips)
+county_facts <- na.omit(county_facts, cols = "state_abbreviation") # Tar bort de rader som innehåller stat- eller landdata
+colnames(county_facts)[1] <- "fips" # Byter namn så att alla ska va samma
+county_facts$fips <- as.character(county_facts$fips) # För att kunna joina måste fips vara av samma class
 results$fips <- as.character(results$fips)
 
-results_county <- full_join(county_facts, results)
-
-crime_data %>%
-  arrange()
-
+results_county <- full_join(county_facts, results) # Lägger samman county_facts och results, matchar med fips
 
 for (i in 1:nrow(crime_data)) {
-  if(crime_data[i, 6] < 10){
+  if(crime_data[i, 6] < 10){ # Om county-koden är mindre än 10, multipliceras stat-koden med 100
     crime_data[i, 5] <- crime_data[i, 5]*100
   }else{
-    if(crime_data[i, 6] < 100 & crime_data[i, 6] > 9){
-    crime_data[i, 5] <- crime_data[i, 5]*10
+    if(crime_data[i, 6] < 100 & crime_data[i, 6] > 9){ # Om county-koden är större än 10 men mindre än 100, multipliceras stat-koden med 10
+      crime_data[i, 5] <- crime_data[i, 5]*10
     }
   }
 }
 
-crime_data <- unite(crime_data, FIPS_ST, FIPS_CTY, col = "fips", sep = "")
-sammanslaget <- full_join(results_county, crime_data)  
+crime_data <- unite(crime_data, FIPS_ST, FIPS_CTY, col = "fips", sep = "") # Lägger ihop stat-koden och county-koden till en gemensam fipskod
+sammanslaget <- full_join(results_county, crime_data)  # Lägger samman results_county och crime_data, matchar med fips
 
 sammanslaget <- sammanslaget %>%
-  select(fips, per_gop_2016, total_votes_2016, everything())
+  select(fips, per_gop_2016, total_votes_2016, everything()) # Flyttar fram kolumner av intresse
 
 
-sammanslaget <- na.omit(sammanslaget, cols = "per_gop_2016")
+sammanslaget <- na.omit(sammanslaget, cols = "per_gop_2016") # Tar bort rader med NA på variabeln andel av rösterna för republikanerna
 
-sammanslaget <- sammanslaget %>%
-  mutate(valdeltagande = total_votes_2016 / (PST045214*(1 - (AGE295214/100)))) %>%
-  select(valdeltagande, everything())
+sammanslaget <- sammanslaget %>% # Lägger till en ny kolumn i datamaterialet, "andel av rösterna för republikanerna" - "andel av rösterna för demokraterna"
+  mutate(gop_win = per_gop_2016 - per_dem_2016) %>%
+  select(gop_win, everything())
 
-ggplot(data = sammanslaget, mapping = aes(x = INC110213, y = per_gop_2016)) +
+for (i in 1:nrow(sammanslaget)) {
+  if(sammanslaget[i, 1] > 0){ # Om gap_win är större än 0, kodas gap_win som 1, annars som 0. 1 = republikansk seger, 0 = demokratisk seger 
+    sammanslaget[i, 1] <- 1
+  }else{
+    sammanslaget[i, 1] <- 0
+  }
+}
+
+
+sammanslaget$gop_win <- as.factor(sammanslaget$gop_win) # Ändrar gap_wins class till factor
+
+
+#  Exploratory data analysis
+scatter1 <- ggplot(data = sammanslaget, mapping = aes(x = INC110213, y = per_gop_2016)) +
   geom_point(color = "red", size = 2) +
-  geom_smooth(method = lm, se = FALSE, color = "blue", size = 1.5) +
+  geom_smooth(method = lm, se = TRUE, color = "blue", size = 1.5) +
   scale_y_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80), labels = c("0%", "20%", "40%", "60%", "80%")) +
   scale_x_continuous(breaks = c(25000, 50000, 75000, 100000, 125000), labels = c("$25000", "$50000", "$75000", "$100000", "$125000")) +
   labs(title = "2016 US presidential election, county-level",
        subtitle = "Final result for the republican party explained by the median income",
        x = "Median household income, 2009-2013",
        y = "Final result for the Republican party")
+scatter1
 
-
-ggplot(data = sammanslaget) + 
+histogram1 <- ggplot(data = sammanslaget) + 
   geom_histogram(mapping = aes(x = per_gop_2016), fill = "red", alpha = 1) +
   geom_histogram(mapping = aes(x = per_gop_2012), fill = "blue", alpha = 0.5) +
   scale_x_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80), labels = c("0%", "20%", "40%", "60%", "80%")) +
@@ -79,24 +79,15 @@ ggplot(data = sammanslaget) +
        x = "Votes in percent",
        y = "Number of counties")
 
-democrats2016 <- sum(sammanslaget$votes_dem_2016)
-democrats2016
-republicans2016 <- sum(sammanslaget$votes_gop_2016)
-republicans2016
+histogram1
 
-ggplot(sammanslaget)+
-  geom_bar(aes(x = sum(sammanslaget$votes_gop_2016)))
-ggplot(data = sammanslaget) +
-  geom_bar(mapping = aes(x = Pclass, fill = Survived)
+boxplot1 <- ggplot(sammanslaget, aes(x = gop_win, y = EDU685213)) +
+  geom_boxplot() +
+  scale_y_continuous(breaks = c(0, 20, 40, 60, 80), labels = c("0%", "20%", "40%", "60%", "80%")) +
+  scale_x_discrete(breaks = c(0, 1), labels = c("Democrats", "Republicans")) +
+  labs(title = "2016 US presidential election",
+       subtitle = "Boxplot of the educational level",
+       x = "Winning party",
+       y = "Bachelor's degree or higher, percent of persons age 25+, 2009-2013")
 
-test <- ggplot(data = senate, mapping = aes(x = presidential_approval, y = election_result)) +
-  geom_point(alpha = 0.3, fill = "black", size = 2) +
-  geom_smooth(method = lm, se = FALSE, color = "red", size = 1.5) +
-  geom_abline(slope = 0, intercept = 0) +
-  scale_y_continuous(breaks = c(-50, -25, 0, 25, 50), labels = c("-50", "-25", "0", "25", "+50")) +
-  scale_x_continuous(breaks = c(20, 30, 40, 50, 60), labels = c("20%", "30", "40", "50", "60")) +
-  labs(title = "Early Presidential Approval And Senate Outcomes?",
-       subtitle = "Senate results vs. January to June approval average, since 2006",
-       caption = c("FIVETHIRTYEIGHT                                                                                      SOURCE: VARIOUS POLLS"),
-       x = "Early presidential approval",
-       y = "Final margin")
+boxplot1
